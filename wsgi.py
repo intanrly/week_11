caf8d823127a245d07df9781aa118109a6ae8eb2
Intan Nurul Laily <intan.nurul.laily-2022@ftmm.unair.ac.id>
2023-11-22 15:53:12 +0700

    A

--- wsgi.py
+++ wsgi.py
@@ -5,5 +5,5 @@ from flasktest import app as application
 
 if __name__ == '__main__':
     host = os.environ.get('FLASK_HOST', '0.0.0.0')
-    port = os.environ.get('FLASK_PORT', '80')
+    port = os.environ.get('FLASK_PORT', '5000')
     application.run(host=host, port=port)
