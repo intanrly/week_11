cdec5bd330b696352f1f33c564c396437585eb3d
Intan Nurul Laily <intan.nurul.laily-2022@ftmm.unair.ac.id>
2023-11-22 14:58:33 +0700

    :a:

--- flasktest.py
+++ flasktest.py
@@ -1,32 +1,32 @@
 from flask import Flask, render_template
-import matplotlib
-matplotlib.use('Agg') #non-interactive backend
 import matplotlib.pyplot as plt
 import io
 import base64
 
-app = Flask(__name__, template_folder='template')
+app = Flask(__name__, template_folder="templates")
 
 def generate_plot():
-    x = [1, 2, 3, 4, 5]
+    x = [1,2,3,4,5]
     y = [i ** 2 for i in x]
 
-    plt.plot(x, y)
-    plt.xlabel('X-axis')
-    plt.ylabel('Y-axis')
-    plt.title('Sample Plot')
+    plt.plot(x,y)
+    plt.xlabel("X-axis")
+    plt.ylabel("Y-axis")
+    plt.title("Sample Plot")
 
     img = io.BytesIO()
-    plt.saveflg(img, format='png')
+    plt.savefig(img, format="png")
     img.seek(0)
 
-    plot_url = base64.b64encode(img.getvalue().decode)
-    plt.close() #close plot
+    plot_url = base64.b64encode(img.getvalue()).decode()
+    plt.close() # Close the plot to avoid conflicts
 
-@app.route('/')
+    return plot_url
+
+@app.route("/")
 def plot():
     plot_url = generate_plot()
-    return render_template('plot.html', plot_url=plot_url)
+    return render_template("plot.html", plot_url=plot_url)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#if __name__ == "__main__":
+#    app.run(debug=True)
